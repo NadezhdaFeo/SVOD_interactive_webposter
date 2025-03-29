1263144db1ab7d2b266b011a558146ad23f26a63
NadezhdaFeo <nvfeofanova@edu.hse.ru>
2025-03-29 09:49:45 +0300

    thirteenth_commit

--- js/index.js
+++ js/index.js
@@ -180,7 +180,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // добавление пина в форму для оценки настроения от 1 до 5
     const form = document.getElementById("pin_adding");
-    form.style.cursor = 'url(../img/pin_cursor.svg), auto';
+    form.style.cursor = 'url(./img/pin_cursor.svg), auto';
     let pinImgUrl = "./img/blue_pin.svg";
     let pinImgOptions = ["./img/blue_pin.svg", "./img/red_pin.svg", "./img/green_pin.svg"];
     form.addEventListener("click", function (e) {
